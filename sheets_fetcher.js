a05ed8266c8e95712fccb5c6719a2506e0c66e11
ShreyanshBanerjee <shreyansh.banerjee.x@gmail.com>
2025-09-28 20:30:36 -0400

    mud monk slime bug fix

--- sheets_fetcher.js
+++ sheets_fetcher.js
@@ -1,6 +1,6 @@
 async function fetchData() {
   try {
-    const url = "https://corsproxy.io/https://docs.google.com/spreadsheets/d/e/2PACX-1vShLix4SLBGRaIInfZdSZl7yiVqOUZaVju3QS3ct_2znOVl9rXNgjd28P9yNnXBNmqulXxtHJMTo6LC/pub?gid=0&single=true&output=csv";
+    const url = "https://corsproxy.io/https://docs.google.com/spreadsheets/d/e/2PACX-1vShLix4SLBGRaIInfZdSZl7yiVqOUZaVju3QS3ct_2znOVl9rXNgjd28P9yNnXBNmqulXxtHJMTo6LC/pub?gid=0&single=true&output=csv&_=" + new Date().getTime();;
     const response = await fetch(url);
     const text = await response.text();
     return text.trim().split("\n").map(r=>r.split(","));
