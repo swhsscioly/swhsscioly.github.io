32c32b32a84cb4205299aad8ab533030cc8fae07
ShreyanshBanerjee <shreyansh.banerjee.x@gmail.com>
2025-12-09 19:59:28 -0500

    yo i figured it out i just changed it to use cloudflares proxy

--- sheets_fetcher.js
+++ sheets_fetcher.js
@@ -1,23 +1,23 @@
 async function fetchData() {
-  const url = "https://thingproxy.freeboard.io/fetch/https://docs.google.com/spreadsheets/d/e/2PACX-1vShLix4SLBGRaIInfZdSZl7yiVqOUZaVju3QS3ct_2znOVl9rXNgjd28P9yNnXBNmqulXxtHJMTo6LC/pub?gid=0&single=true&output=csv&_=" + new Date().getTime();
-  const response = await fetch(url);
-  const text = await response.text();
-  return text.trim().split("\n").map(r => r.split(","));
+  try {
+    const url = "https://sheets-fetcher.jkraus.workers.dev/https://docs.google.com/spreadsheets/d/e/2PACX-1vShLix4SLBGRaIInfZdSZl7yiVqOUZaVju3QS3ct_2znOVl9rXNgjd28P9yNnXBNmqulXxtHJMTo6LC/pub?gid=0&single=true&output=csv&_=" + new Date().getTime();
+    const response = await fetch(url);
+    const text = await response.text();
+    return text.trim().split("\n").map(r=>r.split(",")).filter((a) => {return a[2] == "Yes"});
+  }
+  catch (err) {
+    console.log(err);
+  }
 }
 
 async function fetchProcessData() {
-  const fallback = [["General Info", "https://docs.google.com/document/d/1Y_XDXPSnIZ-nqElenfrqkj9lGlP7dRmJjgecJvEKlrY/edit?tab=t.0"]];
-
-  try {
-    const data = await Promise.race([
-      fetchData(),
-      new Promise((_, r) => setTimeout(() => r("timeout"), 5000))
-    ]);
-
-    const result = data.filter(r => r[2] === "Yes").map(r => [r[0], r[1]]);
-    return result.length ? result : fallback;
-
-  } catch {
-    return fallback;
+  var result = [];
+  const data = await fetchData();
+  for (const row of data) {
+    if (row[2] == "Yes") {
+      result.push([row[0], row[1]]);
+    }
   }
+  
+  return result;
 }
